a12843d421a1f870dc4668c41debc4661f38dc4e
PKCodebase <kaushikprasad1659@gmail.com>
2025-05-22 16:59:10 +0530

    add new classes

---
+++ src/main/java/com/ecommerce/enums/Role.java
@@ -0,0 +1,15 @@
+package com.ecommerce.enums;
+
+import java.util.Collections;
+
+public enum Role {
+
+    USER,
+    SELLER,
+    ADMIN;
+
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + this.name()));
+//    }
+
+}
